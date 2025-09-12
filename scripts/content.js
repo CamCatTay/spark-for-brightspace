35ac68dec87d2d0f95e89be6d66aa99dcc2e29cb
camcattay <182511057+CamCatTay@users.noreply.github.com>
2025-09-11 20:59:54 -0400

    removed redundant url conditional. see manifest

--- scripts/content.js
+++ scripts/content.js
@@ -10,15 +10,13 @@ function getBaseURL() {
 
 async function getCourseInformation() {
     try {
-        if (window.location.href.includes('/d2l/')){
-            let baseURL = getBaseURL();
-                const response = await fetch(baseURL + "/d2l/api/lp/1.43/enrollments/myenrollments/");
-                if (!response.ok) {
-                    throw new Error(`HTTP error. Status: ${response.status}`);
-                }
-                const data = await response.json();
-                console.log(data);
-        }
+        let baseURL = getBaseURL();
+            const response = await fetch(baseURL + "/d2l/api/lp/1.43/enrollments/myenrollments/");
+            if (!response.ok) {
+                throw new Error(`HTTP error. Status: ${response.status}`);
+            }
+            const data = await response.json();
+            console.log(data);
     } catch (error) {
         console.error("Error fetching JSON:", error);
     }

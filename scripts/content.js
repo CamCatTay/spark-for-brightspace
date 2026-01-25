9a9a5aea1bacebea406ea3bd08f47e28776b5b83
camcattay <182511057+CamCatTay@users.noreply.github.com>
2026-01-25 16:33:26 -0500

    basic-ui and refactored content.js
    Co-authored-by: YousefH <yousef-0614@users.noreply.github.com>

--- scripts/content.js
+++ scripts/content.js
@@ -8,19 +8,9 @@ window.addEventListener("load", () => {
     const startTime = performance.now();
     const COURSE_DATA_KEY = "courseData";
     const courseData = {};
+    const oldCourseDataMap = new Map();
 
-    // set courseData from storage if it exists
-    chrome.storage.local.get([COURSE_DATA_KEY], function(result) {
-        if (result.courseData) {
-            Object.assign(courseData, result.courseData);
-            console.log("Course data from storage:", courseData);
-            console.log("It took " + getTimeTaken(startTime, performance.now()) + "s to load stored course data");
-        } else {
-            console.log("No course data found in storage.");
-        }
-    });
-
-
+    // fetch new data first in case it is faster than storage retrieval
     // fetch  course data and update storage
     chrome.runtime.sendMessage({ action: "fetchCourses" }, function(response) {
 
@@ -32,6 +22,25 @@ window.addEventListener("load", () => {
         console.log("Fetched course data:", courseData);
         console.log("It took " + getTimeTaken(startTime, performance.now()) + "s to fetch course data");
     });
+
+    // set courseData from storage if it exists
+    chrome.storage.local.get([COURSE_DATA_KEY], function(result) {
+        if (result.courseData && courseData.isEmpty !== false) {
+            Object.assign(courseData, result.courseData);
+            console.log("Course data from storage:", courseData);
+            console.log("It took " + getTimeTaken(startTime, performance.now()) + "s to load stored course data");
+            console.warn("My dumbass loaded AFTER we got data!");
+        } else {
+            //console.log("No course data found in storage.");
+            // if check is not accurate
+        }
+    });
+
+    // setup UI and create oldCourseDataMap
+    console.warn("My dumbass loaded before we got data!");
+
+    // save course data before unloading/leaving the page (and periodically)
+
 });
 
 chrome.runtime.onMessage.addListener(function(request) {

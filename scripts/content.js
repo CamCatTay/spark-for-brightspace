8ddba54660d3165000422ef28a1d0d64da8f3538
camcattay <182511057+CamCatTay@users.noreply.github.com>
2026-01-25 08:51:43 -0500

    added storage permissions and local storage API

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,36 @@
+function getTimeTaken(startTime, endTime) {
+    return (endTime - startTime).toFixed(2)/1000;
+}
+
 window.addEventListener("load", () => {
     console.log("D2L-Todolist loaded")
+
     const startTime = performance.now();
+    const COURSE_DATA_KEY = "courseData";
+    const courseData = {};
 
+    // set courseData from storage if it exists
+    chrome.storage.local.get([COURSE_DATA_KEY], function(result) {
+        if (result.courseData) {
+            Object.assign(courseData, result.courseData);
+            console.log("Course data from storage:", courseData);
+            console.log("It took " + getTimeTaken(startTime, performance.now()) + "s to load stored course data");
+        } else {
+            console.log("No course data found in storage.");
+        }
+    });
+
+
+    // fetch  course data and update storage
     chrome.runtime.sendMessage({ action: "fetchCourses" }, function(response) {
-        
-        console.log("Course content:", response);
 
-        const endTime = performance.now(); // Record the end time
-        const timeTaken = (endTime - startTime).toFixed(2)/1000;
-        console.log("It took " + timeTaken + "s to fetch course content");
+        // save course data to storage
+        chrome.storage.local.set({ courseData: response }, function() {
+            Object.assign(courseData, response);
+        });
 
+        console.log("Fetched course data:", courseData);
+        console.log("It took " + getTimeTaken(startTime, performance.now()) + "s to fetch course data");
     });
 });
 

7093348a7f72fc33e8b92cca409516d6a32e71b4
camcattay <182511057+CamCatTay@users.noreply.github.com>
2026-01-26 12:35:14 -0500

    implement a way to open side panel

--- scripts/background.js
+++ scripts/background.js
@@ -7,6 +7,32 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             sendResponse(mapToObject(data)); // convert Map to Object for sending via sendResponse
         });
         return true;
-
     }
 });
+
+// open side panel on icon click
+chrome.sidePanel
+.setPanelBehavior({ openPanelOnActionClick: true })
+.catch((error) => console.error(error));
+
+/*
+// disables side panel on non d2l pages (old method, likely to be removed)
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+if (!tab.url) return;
+const url = new URL(tab.url);
+// Enables the side panel on d2l pages
+if (url.pathname.startsWith("/d2l/")) {
+    await chrome.sidePanel.setOptions({
+        tabId,
+        path: "/html/sidepanel.html",
+        enabled: true
+    });
+} else {
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+        tabId,
+        enabled: false
+    });
+}
+});
+*/
